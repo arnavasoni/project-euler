633fe4ba70e36396bf33360d285e54153e56bc7c
Arnava Soni <arnavasoni1@gmail.com>
2024-09-13 23:07:59 +0530

    Commented Logic

--- pandigital_products.py
+++ pandigital_products.py
@@ -1,3 +1,25 @@
+'''
+Here are a few observations:
+The number of digits of a,b,c combined must be equal to 10.
+The term multiplicand (a) and the multiplier (b) are replaceable, i.e., a * b = c and b * a = c. So, while looking for desired combinations of multipliers and multiplicand, both (a, b) and (b, a) will satisfy the condition. However, we have to count them only once as they are practically the same.
+
+Table
+The columns represent the number of digits in b in the tables below. The rows show the number of digits in a, and the value in each cell represents the possible number of digits of c.
+In the table below, there are four highlighted cases. However, this is because the multiplier and multiplicand are replaceable. Thus, we will consider the purple-highlighted cases as two and also as the only possible cases in which the number of digits of a,b,c combined could be equal to 10.
+
+The first case:
+
+a is double digit, b is triple digit, and c should be 5-digit.
+The maximum possible double digit number is: a = 98 and the maximum possible 3-digit number is: b = 987.
+Thus, the upper bound will be a=98 and b=987.
+
+The second case:
+
+a is a single digit, b is 4-digit, and c should be 5-digit.
+The maximum possible single digit is a = 9. The maximum possible 4-digit b = 9876.
+Thus, the upper bound will be a=9 and b=9876.
+'''
+
 sumC = 0
 alreadyFound=[]
 
